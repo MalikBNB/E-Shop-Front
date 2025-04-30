cfc49f2e2b83e9313475f7118cedbe43dd7efaaf
Aymen Abdelmalik <167820077+MalikBNB@users.noreply.github.com>
2025-04-30 23:29:33 +0100

    Get Products List

--- e-shop/src/app/app.component.ts
+++ e-shop/src/app/app.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HttpClient } from '@angular/common/http';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'e-shop';
+  products: IProduct[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<IPagination>('https://localhost:7233/api/products').subscribe(
+      (response) => {
+        this.products = response.data;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 }
---
+++ e-shop/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  imports: [],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.scss'
+})
+export class NavBarComponent implements OnInit {
+
+  ngOnInit(): void {
+
+  }
+}
